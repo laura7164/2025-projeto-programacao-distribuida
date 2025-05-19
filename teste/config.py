ddc8b55c7be05074c7577abc653c1ec549bd1d37
laura-stnz <laura.s.oliveira2018@gmail.com>
2025-05-19 11:18:43 -0300

    Configura integração com MySQL e adiciona .env

--- teste/config.py
+++ teste/config.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__) # criando um site vazio
 
+load_dotenv() # carrega as variáveis do env
+
 app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 # SECRET_KEY -> é usado para proteger dados sensíveis, ele garante que os dados transmitidos entre o servidor e o navegador sejam criptografados
 # SQLALCHEMY_DATABASE_URI -> define qual banco de dados sua aplicação vai usar e onde ele está localizado
